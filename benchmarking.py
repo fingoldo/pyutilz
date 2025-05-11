792aafb679ab5b9f8e9d7ffc2802fe0549c95d89
fingoldo <fingoldo@gmail.com>
2025-05-11 09:19:50 +0300

    Refactoring

--- benchmarking.py
+++ benchmarking.py
@@ -23,7 +23,7 @@ from pyutilz.system import tqdmu
 # ----------------------------------------------------------------------------------------------------------------------------
 
 
-def benchmark_algos_by_runtime(implementations: list, algo_name: str = "", n_reps: int = 2, verbose: int = 0, *args, **kwargs) -> tuple:
+def benchmark_algos_by_runtime(implementations: list, algo_name: str = "", n_reps: int = 2, verbose: int = 0, **algo_kwargs) -> tuple:
     """For a list of implementations on the same algorithm (accepting same inputs),
     repeats them n_reps times and sorts them by min runtime.
     n_reps>=2 means prewarm is possible (good for jit-compiled functions)."""
@@ -37,7 +37,7 @@ def benchmark_algos_by_runtime(implementations: list, algo_name: str = "", n_rep
         min_duration = 1e20
         for rep_id in range(n_reps):
             algo_start = timer()
-            _ = algo(*args, **kwargs)
+            _ = algo(**algo_kwargs)
             duration = timer() - algo_start
             if duration < min_duration:
                 min_duration = duration
@@ -46,7 +46,7 @@ def benchmark_algos_by_runtime(implementations: list, algo_name: str = "", n_rep
             mes_parts.append(f"{algo.__name__}: {duration:.3f} sec.")
 
     if verbose > 1:
-        logger.info(",".join(mes_parts))
+        logger.info("Benchmark timings: " + ",".join(mes_parts))
 
     durations = np.array(durations)
     indices = np.argsort(durations)
